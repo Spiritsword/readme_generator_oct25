c4385fa17cd3989c647c9473dfdf5ec5bbe4c58a
spiritsword <18164236+Spiritsword@users.noreply.github.com>
2025-10-30 08:09:23 +0000

    Tidying file structure

--- python code/main.py
+++ python code/main.py
@@ -5,9 +5,8 @@ from content import content_input
 def main():    
     new_readme_content = content_input()
     readme_markdown = create_readme(new_readme_content)
-    with open("README.md", "w") as f:
+    with open("AUTOREADME.md", "w") as f:
         f.write(readme_markdown)
-    print(readme_markdown)
     
 if __name__ == "__main__":
     main()
